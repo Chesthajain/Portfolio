11e730a97ab0eccb1f4bb46da3af0b31dca65efc
Chestha Jain <cheshtajain1712@gmail.com>
2024-08-24 16:58:23 +0530

    designing

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -30,9 +30,76 @@ const Home = ({ ratio }) => {
             whileInView: {
                 x: 0,
                 opacity: 1,
-            }
-        }
-    }
+            },
+        },
+        button: {
+            initial: {
+                y: "-100%",
+                opacity: 0,
+            },
+            whileInView: {
+                y: 0,
+                opacity: 1,
+            },
+        },
+    };
+    return (
+        <div id="home">
+            <section>
+                <div>
+                    <motion.h1 {...animations.h1}>
+                        Hi, I Am <br />Chestha Jain
+                    </motion.h1>
+
+                    <Typewriter 
+                    options={{
+                        strings: ["Web Developer"],
+                        autoStart: true,
+                        loop: true,
+                        cursor: "",
+                        wrapperClassName: "typewriterpara",
+                    }}
+                    />
+
+                    <div>
+                        <a href="mailto:cheshtajain1712@gmail.com">Hire Me</a>
+                        <a href="#work">Projects<BsArrowUpRight /></a>
+                    </div>
+
+                    <article>
+                        <p>
+                            +
+                            {ratio < 2 &&(
+                                <motion.span
+                                whileInView={animationClientsCount}
+                                ref={clientCount}
+                                ></motion.span>
+                            )}
+                        </p>
+                        <span>Clients Worldwide</span>
+                    </article>
+
+                    <aside>
+                        <article>
+                            <p>
+                                +
+                                {ratio < 2 && (
+                                    <motion.span ref={projectCount}
+                                    whileInView={animationProjectsCount}
+                                    >
+                                        500
+                                    </motion.span>
+                                )}
+                            </p>
+                        </article>
+                    </aside>
+
+
+
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Home
