e09e4b9db006164ec8855f28242c1132e7c0ab5f
Chestha Jain <cheshtajain1712@gmail.com>
2024-08-25 20:15:56 +0530

    continued...

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -3,28 +3,31 @@ import React from 'react';
 const Header = () => {
   return (
     <nav>
-    <NavContent />
+      <NavContent />
     </nav>
   );
 };
 
-const NavContent =() => {
-  <>
-  <h2>Chestha Jain</h2>
-  <div>
-    <a href='#home'>Home</a>
-    <a href='#work'>Work</a>
-    <a href='#timeline'>Experience</a>
-    <a href='#services'>Services</a>
-    <a href='#testimonials'>Testimonials</a>
-    <a href='#contact'>Contact</a>
-    </div>
-    <a href='mailto:cheshtajain1712@gmail.com'>
-      <button>Email</button>
-    </a>
+const NavContent = () => (
+<>
+<h2>Chestha Jain</h2>
+<div>
+<a href='#home'>Home</a>
+<a href='#work'>Work</a>
+<a href='#timeline'>Experience</a>
+<a href='#services'>Services</a>
+<a href='#testimonial'>Testimonial</a>
+<a href='#contact'>Contact</a>
+</div>
 
+<div className='btn'>
+  <a href="mailto: cheshtajain1712@gmail.com">
+    Email
+  </a>
+</div>
 
-  </>
-}
+</>
+  
+)
 
 export default Header;
