d3b2483b42af8be7ca416d71fe8dd76cad8c2fc6
Chestha Jain <cheshtajain1712@gmail.com>
2024-09-01 17:57:33 +0530

    contact

---
+++ portfolio/src/components/Contact.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import vg from "../assets/vg.png";
+import toast from "react-hot-toast";
+import { motion } from "framer-motion";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase";
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [disableBtn, setDisableBtn] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setDisableBtn(true);
+    try {
+      await addDoc(collection(db, "contacts"), {
+        name,
+        email,
+        message,
+      });
+      setName("");
+      setEmail("");
+      setMessage("");
+      toast.success("Message Sent");
+      setDisableBtn(false);
+    }
+};
+
+const animations = {
+    
+}
